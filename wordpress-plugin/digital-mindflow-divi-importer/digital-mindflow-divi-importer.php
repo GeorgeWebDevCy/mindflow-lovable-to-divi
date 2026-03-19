f1774084eb9c8dbf2ad0c84f424f03842c6aa9f4
George Nicolaou <orionas.elite@gmail.com>
2026-03-19 15:00:42 +0200

    Widen homepage hero title

--- wordpress-plugin/digital-mindflow-divi-importer/digital-mindflow-divi-importer.php
+++ wordpress-plugin/digital-mindflow-divi-importer/digital-mindflow-divi-importer.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Digital MindFlow Divi Importer
  * Description: WP admin importer for the Digital MindFlow Divi 5 layouts, Theme Builder template, primary navigation, and native Divi portfolio loop fix.
- * Version: 0.1.87
+ * Version: 0.1.88
  * Author: George Nicolaou
  */
 
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DMF_DIVI_IMPORTER_VERSION', '0.1.87' );
+define( 'DMF_DIVI_IMPORTER_VERSION', '0.1.88' );
 define( 'DMF_DIVI_IMPORTER_FILE', __FILE__ );
 define( 'DMF_DIVI_IMPORTER_DIR', plugin_dir_path( __FILE__ ) );
 define( 'DMF_DIVI_IMPORTER_URL', plugin_dir_url( __FILE__ ) );
